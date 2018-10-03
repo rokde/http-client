75d56ae7d1b36fe46319696c1219a89f726fe0f8
rokde <github@mail.robert-kummer.de>
2018-10-03 11:50:22 +0200

    psr-2 compliance

--- src/Response.php
+++ src/Response.php
@@ -4,115 +4,123 @@ namespace Rokde\HttpClient;
 
 class Response
 {
-	/**
-	 * @var string response body
-	 */
-	private $content;
+    /**
+     * @var string response body
+     */
+    private $content;
 
-	/**
-	 * @var array|Header[] header collection
-	 */
-	private $headers = [];
+    /**
+     * @var array|Header[] header collection
+     */
+    private $headers = [];
 
-	/**
-	 * @var int initial response status code
-	 */
-	private $status = 0;
+    /**
+     * @var int initial response status code
+     */
+    private $status = 0;
 
-	/**
-	 * @var null|string inital response status message
-	 */
-	private $status_message = null;
+    /**
+     * @var null|string inital response status message
+     */
+    private $status_message = null;
 
-	/**
-	 * @var string initial protocal version
-	 */
-	private $protocolVersion;
+    /**
+     * @var string initial protocal version
+     */
+    private $protocolVersion;
 
-	/**
-	 * do not instantiate it directly
-	 */
-	private function __construct()
-	{
-	}
+    /**
+     * do not instantiate it directly
+     */
+    private function __construct()
+    {
+    }
 
-	/**
-	 * factory creation of a response
-	 *
-	 * @param array $responseHeaderLines
-	 * @param string $content
-	 * @return Response
-	 */
-	public static function create(array $responseHeaderLines, string $content): self
-	{
-		$response = new static();
-		$response->content = $content;
+    /**
+     * factory creation of a response
+     *
+     * @param  array $responseHeaderLines
+     * @param  string $content
+     * @return Response
+     */
+    public static function create(array $responseHeaderLines, string $content): self
+    {
+        $response = new static();
+        $response->content = $content;
 
-		foreach ($responseHeaderLines as $headerLine) {
-			try {
-				$header = Header::fromString($headerLine);
-				$response->headers[$header->name()] = $header;
-			} catch (\InvalidArgumentException $e) {
-				if (preg_match('~^HTTP\/([\d\.]*)\ (\d{3})\ (.*)$~', $headerLine, $matches)) {
-					$response->protocolVersion = $matches[1];
-					$response->status = $matches[2] + 0;
-					$response->status_message = $matches[3];
-				}
-			}
-		}
+        foreach ($responseHeaderLines as $headerLine) {
+            try {
+                $header = Header::fromString($headerLine);
+                $response->headers[$header->name()] = $header;
+            } catch (\InvalidArgumentException $e) {
+                if (preg_match('~^HTTP\/([\d\.]*)\ (\d{3})\ (.*)$~', $headerLine, $matches)) {
+                    $response->protocolVersion = $matches[1];
+                    $response->status = $matches[2] + 0;
+                    $response->status_message = $matches[3];
+                }
+            }
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 
-	public function status(): int
-	{
-		return $this->status;
-	}
+    public function json()
+    {
+        if (!extension_loaded('json')) {
+            throw new \RuntimeException('The necessary json extension is not loaded');
+        }
 
-	public function content(): string
-	{
-		return $this->content;
-	}
+        return json_decode($this->content(), true);
+    }
 
-	public function json()
-	{
-		if (!extension_loaded('json')) {
-			throw new \RuntimeException('The necessary json extension is not loaded');
-		}
+    public function __toString(): string
+    {
+        $headers = '';
+        /** @var Header $header */
+        foreach ($this->headers as $header) {
+            $headers .= $header->valueLine();
+        }
 
-		return json_decode($this->content(), true);
-	}
+        return 'HTTP/' . $this->protocolVersion . ' ' . $this->status() . ' ' . $this->status_message . "\r\n"
+            . $headers . "\r\n"
+            . $this->content();
+    }
 
-	public function __toString(): string
-	{
-		$headers = '';
-		/** @var Header $header */
-		foreach ($this->headers as $header) {
-			$headers .= $header->valueLine();
-		}
+    public function status(): int
+    {
+        return $this->status;
+    }
 
-		return 'HTTP/' . $this->protocolVersion . ' ' . $this->status() . ' ' . $this->status_message . "\r\n"
-			. $headers . "\r\n"
-			. $this->content();
-	}
+    public function isOk(): bool
+    {
+        return $this->status() >= 200 && $this->status() < 300;
+    }
 
-	public function isOk(): bool
-	{
-		return $this->status() >= 200 && $this->status() < 300;
-	}
+    public function isRedirect(): bool
+    {
+        return $this->status() >= 300 && $this->status() < 400;
+    }
 
-	function isRedirect(): bool
-	{
-		return $this->status() >= 300 && $this->status() < 400;
-	}
+    public function isClientError(): bool
+    {
+        return $this->status() >= 400 && $this->status() < 500;
+    }
 
-	function isClientError(): bool
-	{
-		return $this->status() >= 400 && $this->status() < 500;
-	}
+    public function isServerError(): bool
+    {
+        return $this->status() >= 500;
+    }
 
-	function isServerError(): bool
-	{
-		return $this->status() >= 500;
-	}
+    /**
+     * @return array|Header[] header collection
+     */
+    public function headers(): array
+    {
+        return $this->headers;
+    }
+
+    public function header(string $name, $default = null)
+    {
+        return array_key_exists($name, $this->headers) ? $this->headers[$name] : $default;
+    }
 }
