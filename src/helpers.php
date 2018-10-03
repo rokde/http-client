8494d85fc07f369dcdb37e1e6aa7cd4bc975ecce
rokde <github@mail.robert-kummer.de>
2018-10-03 11:47:08 +0200

    psr-2 compliance

--- src/helpers.php
+++ src/helpers.php
@@ -1,24 +1,22 @@
 <?php
 
 if (!function_exists('http')) {
+    /**
+     * function helper to retrieve an http request as easy as pie
+     *
+     * @param  string $url
+     * @param  string $method
+     * @param  array $headers
+     * @return \Rokde\HttpClient\Response|\Rokde\HttpClient\Http
+     */
+    function http(string $url = null, string $method = 'GET', array $headers = [])
+    {
+        $http = new \Rokde\HttpClient\Http($url, $method, $headers);
 
-	/**
-	 * function helper to retrieve an http request as easy as pie
-	 *
-	 * @param string $url
-	 * @param string $method
-	 * @param array $headers
-	 * @return \Rokde\HttpClient\Response|\Rokde\HttpClient\Http
-	 */
-	function http(string $url = null, string $method = 'GET', array $headers = [])
-	{
-		$http = new \Rokde\HttpClient\Http($url, $method, $headers);
+        if ($url === null) {
+            return $http;
+        }
 
-		if ($url === null) {
-			return $http;
-		}
-
-		return $http->send();
-	}
-
+        return $http->send();
+    }
 }
